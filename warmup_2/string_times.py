551ac02197b2aa9e94d1504e5183d1d932aaa8d6
Farah Rubena <82815385+farah-rubena@users.noreply.github.com>
2023-06-13 16:15:15 +0530

    Add files via upload

---
+++ warmup_2/string_times.py
@@ -0,0 +1,11 @@
+'''Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+'''
+
+def stringtimes(text, n):
+
+    return text * n 
+
+print(stringtimes('Hi', 2))
+print(stringtimes('Hi', 3))
+print(stringtimes('Hi', 1))
