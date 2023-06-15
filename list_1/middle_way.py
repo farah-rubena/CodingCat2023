0e32d4d0f51230a650196844340a41dab7156868
Farah Rubena <82815385+farah-rubena@users.noreply.github.com>
2023-06-15 18:47:17 +0530

    Add files via upload

---
+++ list_1/middle_way.py
@@ -0,0 +1,11 @@
+'''Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+'''
+
+def middleway(a,b):
+
+    return a[1]b[1]
+
+print(middleway([1, 2, 3], [4, 5, 6]))
+print(middleway([7, 7, 7], [3, 8, 0]))
+print(middleway([5, 2, 9], [1, 4, 5]))
