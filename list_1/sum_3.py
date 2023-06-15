517d72e56f9ba2b01be1a04c40f37ccf74d96c63
Farah Rubena <82815385+farah-rubena@users.noreply.github.com>
2023-06-15 17:51:09 +0530

    Add files via upload

---
+++ list_1/sum_3.py
@@ -0,0 +1,12 @@
+'''Given an array of ints length 3, return the sum of all the elements.
+
+'''
+
+def sum3(num_list):
+
+    return num_list[0] + num_list[1] + num_list[2]
+
+print(sum3([1, 2, 3]))
+print(sum3([5, 11, 2]))
+print(sum3([7, 0, 0]))
+
