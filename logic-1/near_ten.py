9c1743b1b764237d71fb3b27169c75d1eef70457
Farah Rubena <82815385+farah-rubena@users.noreply.github.com>
2023-06-16 14:06:37 +0530

    Add files via upload

---
+++ logic-1/near_ten.py
@@ -0,0 +1,17 @@
+'''Given a non-negative number "num", 
+return True if num is within 2 of a multiple of 10. 
+Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
+'''
+
+
+def nearten(num):
+
+    if num%10 <=2 or num%10 >=8:
+        return True
+    else:
+        return False
+    
+print(nearten(12))
+print(nearten(17))
+print(nearten(19))
+
