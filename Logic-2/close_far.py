9d4c0bb95561109d8ebb9abf169f5aff625a6451
Farah Rubena <82815385+farah-rubena@users.noreply.github.com>
2023-06-17 16:19:39 +0530

    Add files via upload

---
+++ Logic-2/close_far.py
@@ -0,0 +1,13 @@
+'''Given three ints, a b c, return True if one of b or c is "close" (differing from a by at most 1), 
+while the other is "far", differing from both other values by 2 or more. 
+Note: abs(num) computes the absolute value of a number.
+'''
+
+
+def closefar(a,b,c):
+
+    if abs(a - b) <= 1 and abs(b - c) >= 2 and abs(a - c) >= 2:
+        return True
+    if abs(a - c) <= 1 and abs(a - b) >= 2 and abs(b - c) >= 2:
+        return True
+    return False
